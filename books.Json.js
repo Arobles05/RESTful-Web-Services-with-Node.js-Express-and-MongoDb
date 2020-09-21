0a56d6811b70b5a8fc6dd93d0327fc7418e6d998
Angel Robles <angelrobles05@gmail.com>
2020-09-20 23:33:07 -0400

    finalizacion del api

---
+++ books.Json.js
@@ -0,0 +1,19 @@
+db.books.insert([{
+        title: "Man and Black",
+        genre: "Fiction",
+        author: " i dont know production",
+        read: false,
+    },
+    {
+        title: "Advennger",
+        genre: "Fiction",
+        author: "MARVEL",
+        read: false,
+    },
+    {
+        title: "Justice Leagues",
+        genre: "Fiction",
+        author: "DC COMIC",
+        read: false,
+    },
+]);
